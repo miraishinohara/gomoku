d493f1d2685ce933b437b2192ab274c49ad1f719
miraishinohara <miraiminato8@yahoo.co.jp>
2016-07-27 22:02:57 +0900

    Update and rename README.md to source/Game.cs

---
+++ source/Game.cs
@@ -0,0 +1,13 @@
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+ class Game
+    {
+        static void Main(string[] args)
+        {
+            Gomoku gomoku = new Gomoku();
+            gomoku.Start();
+        }
+    }
